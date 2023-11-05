836317059a6e2206099a202a9ce36514e7eb20c3
ryuto3384 <ryuto.1128.n@icloud.com>
2023-11-05 12:47:57 +0900

    search bar commit

--- FakeMobileMac/View/OrderView.swift
+++ FakeMobileMac/View/OrderView.swift
@@ -9,113 +9,108 @@ import SwiftUI
 import MapKit
 
 extension CLLocationCoordinate2D {
-    static let parking = CLLocationCoordinate2D(latitude: 35.264406848968754, longitude: 137.04344647983666)
+    static let myLocation = CLLocationCoordinate2D(latitude: 35.264406848968754, longitude: 137.04344647983666)
 }
 
 extension MKCoordinateRegion {
-    static let boston = MKCoordinateRegion(
-        center: CLLocationCoordinate2D(
-            latitude: 42.360256, longitude: -71.057279
-        ),
-        span: MKCoordinateSpan(
-            latitudeDelta: 0.1, longitudeDelta: 0.1
-        )
-    )
-    static let northShore = MKCoordinateRegion(
-        center: CLLocationCoordinate2D(
-            latitude: 42.547408, longitude: -70.870085
-        ),
-        //領域？
-        span: MKCoordinateSpan(
-            latitudeDelta: 0.5, longitudeDelta: 0.5
-        )
-    )
+    static var myRegion: MKCoordinateRegion {
+        return .init(center: .myLocation, latitudinalMeters: 1000, longitudinalMeters: 1000)
+    }
 }
 
 struct OrderView: View {
     
+    @State private var position: MapCameraPosition = .region(.myRegion)
+    @State private var selectedResult: MKMapItem?
+    //buttonのアニメーションを追加するもの
+    //押されると選択されてる感じが出る
+    @Namespace private var locationSpace
+    @State private var viewingRegion: MKCoordinateRegion?
+    
+    @State private var searchText: String = ""
+    @State private var showSearch: Bool = false
     @State private var searchResults: [MKMapItem] = []
     
-    @State private var visibleRegion: MKCoordinateRegion?
-    
-    @State private var position: MapCameraPosition = .automatic
-    
-    @State private var selectedResult: MKMapItem?
+    @State private var showDetaiols: Bool = false
+    @State private var lookAroundScene: MKLookAroundScene?
+   
+    @State private var routeDisplaying: Bool = false
     @State private var route: MKRoute?
+    @State private var routeDestination: MKMapItem?
     
     var body: some View {
         
-        Map(position: $position, selection: $selectedResult){
-            Annotation("Station", coordinate: .parking) {
-                ZStack {
-                    RoundedRectangle(cornerRadius: 5)
-                        .fill(.background)
-                    RoundedRectangle(cornerRadius: 5)
-                        .stroke(.secondary, lineWidth: 5)
-                    Image(systemName: "train.side.rear.car")
-                        .padding(5)
+        NavigationStack {
+            Map(position: $position, selection: $selectedResult, selection: $selectedResult){
+                Annotation("Station", coordinate: .myLocation) {
+                    ZStack {
+                        RoundedRectangle(cornerRadius: 5)
+                            .fill(.background)
+                        RoundedRectangle(cornerRadius: 5)
+                            .stroke(.secondary, lineWidth: 5)
+                        Image(systemName: "train.side.rear.car")
+                            .padding(5)
+                    }
                 }
-            }
-            .annotationTitles(.hidden)
-            
-            ForEach(searchResults, id: \.self) { result in
-                Marker(item: result)
-            }
-            .annotationTitles(.hidden)
-            
-            UserAnnotation()
-            
-        }
-        .mapStyle(.hybrid(elevation: .realistic))
-        .safeAreaInset(edge: .bottom) {
-            HStack{
-                Spacer()
-                VStack(spacing: 0){
+                .annotationTitles(.hidden)
+                
+                ForEach(searchResults, id: \.self) { result in
+                    let placemark = result.placemark
+                    Marker(placemark.name ?? "place", coordinate: placemark.coordinate)
                     
-                    
-                    BeantownButtons(position: $position, searchResults: $searchResults, visibleRegioon: visibleRegion)
-                        .padding(.top)
                 }
-                Spacer()
+                .annotationTitles(.hidden)
+                
+                UserAnnotation()
                 
             }
-            .background(.thinMaterial)
-        }
-        .onChange(of: searchResults) {
-            position = .automatic
-        }
-        .onChange(of: selectedResult) {
-            getDirections()
-        }
-        .onMapCameraChange { context in
-            visibleRegion = context.region
-        }
-        //現在地ボタン
-        .mapControls {
-            MapUserLocationButton()
-            MapCompass()
-            MapScaleView()
-        }
-        
-        
-    }
-    
-    func getDirections() {
-        route = nil
-        guard let selectedResult else { return }
-        
-        let request = MKDirections.Request()
-        request.source  = MKMapItem(placemark: MKPlacemark(coordinate: .parking))
-        request.destination = selectedResult
-        
-        Task{
-            let directions = MKDirections(request: request)
-            let response = try? await directions.calculate()
+            .mapStyle(.standard(elevation: .realistic))
             
-            route = response?.routes.first
+            //現在地などのボタン
+            .overlay(alignment: .bottomTrailing) {
+                VStack(spacing: 15) {
+                    MapUserLocationButton(scope: locationSpace)
+                    MapCompass(scope: locationSpace)
+                    MapPitchToggle(scope: locationSpace)
+                }
+            }
+            .mapScope(locationSpace)
+            .navigationTitle("モバイルオーダー")
+            .navigationBarTitleDisplayMode(.inline)
+            
+            //searchbar
+            .searchable(text: $searchText, isPresented: $showSearch)
+            
+            //toolbarのデザイン
+            .toolbarBackground(.visible, for: .navigationBar)
+            .toolbarBackground(.ultraThinMaterial, for: .navigationBar)
         }
+        //検索結果を送信&取得
+        .onSubmit {
+            //Task内は非同期処理を行える
+            Task {
+                guard !searchText.isEmpty else {return}
+                await searchPlaces()
+            }
+        }
+        
+        //アプリ起動時に位置情報の許可
+        .onAppear {
+            CLLocationManager().requestWhenInUseAuthorization()
+        }
+        
+        
+        
     }
     
+    func searchPlaces() async {
+        let request = MKLocalSearch.Request()
+        request.naturalLanguageQuery = searchText
+        request.region = .myRegion
+        
+        let results = try? await MKLocalSearch(request: request).start()
+        searchResults = results?.mapItems ?? []
+    }
     
 }
 
